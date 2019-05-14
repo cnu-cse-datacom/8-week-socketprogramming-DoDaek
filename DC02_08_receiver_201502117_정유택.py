597c1c69b0d468878c32ce21be98505b38be0fcd
DoDaek <dbxor8960@naver.com>
2019-05-15 00:23:13 +0900

    Change the option of open file

--- DC02_08_receiver_201502117_정유택.py
+++ DC02_08_receiver_201502117_정유택.py
@@ -6,7 +6,7 @@ socket.bind(('', 9000))
 
 recv_file_name, addr = socket.recvfrom(2000)
 file_name = recv_file_name.decode()
-sleep(0.1)
+sleep(0.5)
 
 recv_size, _ = socket.recvfrom(2000)
 total_size = int(recv_size)
@@ -15,14 +15,13 @@ print('file recv start from', addr[0])
 print('File Name :', file_name)
 print('File size :', total_size)
 
-with open(file_name, 'w') as recv_file:
+with open(file_name, 'wb') as recv_file:
     current_size = 0
     while current_size != total_size:
         current_size = current_size + 1024
         if current_size > total_size:
             current_size = total_size
         divided_file, _ = socket.recvfrom(1024)
-        divided_file = str(divided_file.decode())
         recv_file.write(divided_file)
         percent = str((current_size / total_size) * 100) + '%'
         print('current_size / total_size =', current_size, '/', total_size, ',', percent)
